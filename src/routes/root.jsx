42eb355584293f84c9de71c0c7fe47db4be3441e
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-01 22:23:52 -0300

    react

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,31 @@
 export default function Root(){
     return(
         <div>
-            
+            <div id="sidebar">
+                <h1>React Router Contracts</h1>
+            </div>
+            <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+
         </div>
     )
 }

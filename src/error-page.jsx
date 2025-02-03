37619117788d0e316c46c5d08f21df8cec4e6316
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-02 22:07:40 -0300

    react

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -3,4 +3,11 @@ import { useRouteError } from "react-router-dom";
 export default function ErrorPage(){
     const error = useRouteError()
     console.log(error)
+
+    return(
+        <div id="error-page">
+            <h1>Ooops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+        </div>
+    )
 }

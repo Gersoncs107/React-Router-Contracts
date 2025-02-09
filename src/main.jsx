8c2b831a064275c2c9fa6881fbea84a8bd089681
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-08 22:01:14 -0300

    react

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Root, {loader as rootLoader} from './routes/root'
+import Root, {loader as rootLoader, action as rootAction} from './routes/root'
 import ErrorPage from './error-page'
 import Contact from './routes/contact'
 

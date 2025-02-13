d9d65873c7d17205036b39e411a488dccd3e27cf
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-13 19:48:41 -0300

    react

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -1,2 +1,7 @@
 import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
+
+export async function action({params}) {
+    await deleteContact(params.concatctId)
+    return redirect("/")
+}

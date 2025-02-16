483c872a570be812c0b880f83814a970981afb78
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-15 23:11:24 -0300

    react

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,12 @@
 export default function Index(){
     return(
-
+        <p id="zero-state">
+            <br />
+            Check out{" "}
+            <a href="https://reactrouter.com">
+                the docs ate reactrouter.com
+            </a>
+            .
+        </p>
     )
 }

84ed62e8c717dd9fa302e0999193b68092c40076
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-16 22:28:33 -0300

    react

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,6 +7,8 @@ export async function action() {
 }
 
 export async function loader() {
+  const url = new URL(request.url)
+  const q = url.searchParams.get("q")
   const contacts = await getContacts();
   return { contacts };
 }
@@ -19,7 +21,7 @@ export default function Root() {
         <div id="sidebar">
           <h1>React Router Contacts</h1>
           <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
@@ -36,7 +38,7 @@ export default function Root() {
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
             <button type="submit">New</button>
             </Form>

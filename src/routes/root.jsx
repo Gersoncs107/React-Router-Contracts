f17c6a92661e12380ea84e1b83a6362971570d2c
Gersoncs107 <gerson.silva107@gmail.com>
2025-02-17 22:55:08 -0300

    react

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -10,7 +10,7 @@ export async function loader({ request }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 
